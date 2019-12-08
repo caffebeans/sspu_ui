54e5ce2001645d05d3910fce0e47c17df5a7d3cb
admode <fjut.zhangliang@gmail.com>
2019-12-08 20:56:28 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import store from './store/'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 
+
+
+
+
 Vue.config.productionTip = false;
-
 Vue.use(ElementUI);
-
 new Vue({
 	el: '#app',
 	router,
--- src/page/login.vue
+++ src/page/login.vue
@@ -1,36 +1,37 @@
 <template>
-  	<div class="login_page fillcontain">
-	  	<transition name="form-fade" mode="in-out">
-	  		<section class="form_contianer" v-show="showLogin">
-		  		<div class="manage_tip">
-		  			<p>后台管理系统</p>
-		  		</div>
-		    	<el-form :model="loginForm" :rules="rules" ref="loginForm">
-					<el-form-item prop="username">
-						<el-input v-model="loginForm.username" placeholder="用户名"><span>dsfsf</span></el-input>
-					</el-form-item>
-					<el-form-item prop="password">
-						<el-input type="password" placeholder="密码" v-model="loginForm.password"></el-input>
-					</el-form-item>
-					<el-form-item>
-				    	<el-button type="primary" @click="submitForm('loginForm')" class="submit_btn">登陆</el-button>
-				  	</el-form-item>
-					<el-form-item>
-				    	<el-button type="primary" @click="register('loginForm')" class="submit_btn">注册</el-button>
-				  	</el-form-item>
-				</el-form>
-	  		</section>
-	  	</transition>
-  	</div>
+    <div class="login_page fillcontain">
+        <transition name="form-fade" mode="in-out">
+            <section class="form_contianer" v-show="showLogin">
+                <div class="manage_tip">
+                    <p>后台管理系统</p>
+                </div>
+                <el-form :model="loginForm" :rules="rules" ref="loginForm">
+                    <el-form-item prop="username">
+                        <el-input v-model="loginForm.username" placeholder="用户名"><span>dsfsf</span></el-input>
+                    </el-form-item>
+                    <el-form-item prop="password">
+                        <el-input type="password" placeholder="密码" v-model="loginForm.password"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="submitForm('loginForm')" class="submit_btn">登陆</el-button>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="register('loginForm')" class="submit_btn">注册</el-button>
+                    </el-form-item>
+                </el-form>
+            </section>
+        </transition>
+    </div>
 </template>
 
 <script>
 	import {login, getAdminInfo} from '@/api/getData'
 	import {mapActions, mapState} from 'vuex'
+
 	const axios = require('axios');
 
 	export default {
-	    data(){
+		data() {
 			return {
 				loginForm: {
 					username: '',
@@ -38,20 +39,20 @@
 				},
 				rules: {
 					username: [
-			            { required: true, message: '请输入用户名', trigger: 'blur' },
-			        ],
+						{required: true, message: '请输入用户名', trigger: 'blur'},
+					],
 					password: [
-						{ required: true, message: '请输入密码', trigger: 'blur' }
+						{required: true, message: '请输入密码', trigger: 'blur'}
 					],
 				},
 				showLogin: false,
 			}
 		},
-		mounted(){
+		mounted() {
 			this.showLogin = true;
 			if (!this.adminInfo.id) {
-    			this.getAdminData()
-    		}
+				this.getAdminData()
+			}
 		},
 		computed: {
 			...mapState(['adminInfo']),
@@ -59,42 +60,34 @@
 		methods: {
 			...mapActions(['getAdminData']),
 			async submitForm(formName) {
+                  let that=this;
+				axios.post('http://localhost:8888/login', {
+					username: this.loginForm.username,
+					password: this.loginForm.password
+				}).then(function (response) {
 
-				alert(this.loginForm.username);
-			    axios.post('http://localhost:8888/login', {
-                username: this.loginForm.username,
-                password: this.loginForm.password
-              }).then(function (response) {
-     
-               if(response.data.code=="200"){
-                 alert("dddddd");
-                 this.$router.push('/manage');
-             }
-        })
-  .catch(function (error) {
-    console.log(error);
-  });
-
-// Option
-                  
-
-
-
+					if (response.data.code == "200") {
+						that.$router.push('/manage');
+					}
+				})
+					.catch(function (error) {
+						console.log(error);
+					});
 			},
-			 register(formName) {
-				 alert("ddd")
-			     this.$router.push("/register")
+			register(formName) {
+				alert("ddd")
+				this.$router.push("/register")
 			},
-			
+
 		},
 
 		watch: {
-			adminInfo: function (newValue){
+			adminInfo: function (newValue) {
 				// if (newValue.id) {
 				// 	this.$message({
-                //         type: 'success',
-                //         message: '检测到您之前登录过，将自动登录'
-                //     });
+				//         type: 'success',
+				//         message: '检测到您之前登录过，将自动登录'
+				//     });
 				// 	this.$router.push('manage')
 				// }
 			}
@@ -103,41 +96,49 @@
 </script>
 
 <style lang="less" scoped>
-	@import '../style/mixin';
-	.login_page{
-		background-color: #324057;
-	}
-	.manage_tip{
-		position: absolute;
-		width: 100%;
-		top: -100px;
-		left: 0;
-		p{
-			font-size: 34px;
-			color: #fff;
-		}
-	}
-	.form_contianer{
-		.wh(320px, 210px);
-		.ctp(320px, 210px);
-		padding: 25px;
-		border-radius: 5px;
-		text-align: center;
-		background-color: #fff;
-		.submit_btn{
-			width: 100%;
-			font-size: 16px;
-		}
-	}
-	.tip{
-		font-size: 12px;
-		color: red;
-	}
-	.form-fade-enter-active, .form-fade-leave-active {
-	  	transition: all 1s;
-	}
-	.form-fade-enter, .form-fade-leave-active {
-	  	transform: translate3d(0, -50px, 0);
-	  	opacity: 0;
-	}
+    @import '../style/mixin';
+
+    .login_page {
+        background-color: #324057;
+    }
+
+    .manage_tip {
+        position: absolute;
+        width: 100%;
+        top: -100px;
+        left: 0;
+
+        p {
+            font-size: 34px;
+            color: #fff;
+        }
+    }
+
+    .form_contianer {
+        .wh(320px, 210px);
+        .ctp(320px, 210px);
+        padding: 25px;
+        border-radius: 5px;
+        text-align: center;
+        background-color: #fff;
+
+        .submit_btn {
+            width: 100%;
+            font-size: 16px;
+        }
+    }
+
+    .tip {
+        font-size: 12px;
+        color: red;
+    }
+
+    .form-fade-enter-active, .form-fade-leave-active {
+        transition: all 1s;
+    }
+
+    .form-fade-enter, .form-fade-leave-active {
+        transform: translate3d(0, -50px, 0);
+        opacity: 0;
+    }
 </style>
